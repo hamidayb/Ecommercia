485e032925142cdd0948ca0f4aad18e8a392da0d
hamidayb <hayub.bese18seecs@seecs.edu.pk>
2021-07-28 13:02:00 +0500

    Implemented ProductScreen Redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,14 @@
 import { applyMiddleware, combineReducers, createStore } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
-import { productListReducer } from "./reducers/productReducers"
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers"
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 })
 const initialState = {}
 
