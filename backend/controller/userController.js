eaa93122759bbd443402ab62a1282f62b0b8570c
hamidayb <hayub.bese18seecs@seecs.edu.pk>
2021-08-07 13:37:09 +0500

    created update endpoint

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -75,3 +75,29 @@ export const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error("User not found")
   }
 })
+
+// @desc    Update a user
+// @route   PUT /api/users/profile
+// @access  private
+export const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findOne(req.user._id)
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password || user.password
+    }
+
+    const updatedUser = await user.save()
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updatedUser.email,
+      isAdmin: updateUser.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
+})
