baeadc77d196ada4838c9382babad8dd38a3d6b5
hamidayb <hayub.bese18seecs@seecs.edu.pk>
2021-08-07 12:19:35 +0500

    Register Screen created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from "./screens/ProductScreen"
 import CartScreen from "./screens/CartScreen"
 import LoginScreen from "./screens/LoginScreen"
+import RegisterScreen from "./screens/RegisterScreen"
 import { Container } from "react-bootstrap"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import "./bootstrap.min.css"
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/login" component={LoginScreen} />
+          <Route path="/register" component={RegisterScreen} />
         </Container>
       </main>
       <Footer />
