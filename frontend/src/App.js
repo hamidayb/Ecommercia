accffb9a81efe0967e3dd937ee5eebe008f21e70
hamidayb <hayub.bese18seecs@seecs.edu.pk>
2021-08-08 14:10:19 +0500

    Payment Screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import LoginScreen from "./screens/LoginScreen"
 import RegisterScreen from "./screens/RegisterScreen"
 import ProfileScreen from "./screens/ProfileScreen"
 import ShippingScreen from "./screens/ShippingScreen"
+import PaymentScreen from "./screens/PaymentScreen"
 import { Container } from "react-bootstrap"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import "./bootstrap.min.css"
@@ -24,6 +25,8 @@ const App = () => {
           <Route path="/register" component={RegisterScreen} />
           <Route path="/profile" component={ProfileScreen} />
           <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/payment" component={PaymentScreen} />
+          {/* <Route path="/placeorder" component={PlaceOrderScreen} /> */}
         </Container>
       </main>
       <Footer />
